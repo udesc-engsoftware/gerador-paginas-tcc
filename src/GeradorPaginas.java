d9c3f97fecb0645031c02125a557686f575b553e
ErickWarmling <warmling.erick@gmail.com>
2025-02-26 16:23:39 -0300

    Métodos gerarNomeArquivo e gerarArquivoTemplate

--- src/GeradorPaginas.java
+++ src/GeradorPaginas.java
@@ -1,7 +1,60 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class GeradorPaginas {
 
     public static void main(String[] args) {
-       
+        String arquivoCsv = "arquivo.txt";
+        String template = "template.md";
+    }
+
+    private static void lerCsv (String arquivoCsv, String template) {
+        try (BufferedReader br = new BufferedReader(new FileReader(arquivoCsv))){
+            br.readLine();
+            String linhaTcc;
+
+            while ((linhaTcc = br.readLine()) != null) {
+                String[] dadosPlanilha = linhaTcc.split(",");
+                String nomeArquivo = gerarNomeArquivo(dadosPlanilha[5], dadosPlanilha[0]);
+                String templateArquivo = gerarArquivoTemplate(template, dadosPlanilha);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String gerarNomeArquivo (String aluno, String semestre) {
+        return semestre.replace("/", "-") + "-" + aluno.toLowerCase().replace(" ", "-") + ".md";
+    }
+
+    private static String gerarArquivoTemplate (String template, String[] dadosPlanilha) {
+        StringBuilder sb = new StringBuilder();
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(template))) {
+            String linhaTemplate;
+
+            while ((linhaTemplate = bufferedReader.readLine()) != null) {
+                linhaTemplate = linhaTemplate.replace("$titulo$", dadosPlanilha[1].trim())
+                                             .replace("$nome", dadosPlanilha[0].trim())
+                                             .replace("$area$", dadosPlanilha[4].trim())
+                                             .replace("$data$", dadosPlanilha[6].trim())
+                                             .replace("$hora$", dadosPlanilha[7].trim())
+                                             .replace("$local$", dadosPlanilha[8].trim())
+                                             .replace("$orientador$", dadosPlanilha[2].trim())
+                                             .replace("$membros$", dadosPlanilha[3].trim());
+
+                sb.append(linhaTemplate).append("\n");
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return sb.toString();
     }
 
 }
---
+++ src/template.md
@@ -0,0 +1,27 @@
+---
+layout: page-fullwidth
+title: ""
+meta_title: "Engenharia de Software"
+subheadline: ""
+teaser: ""
+header:
+   image_fullwidth: "banner-site-eso.png"
+---
+
+## $titulo$
+
+#### Aluno(a): $nome$
+
+#### Área: $area$
+
+#### Data: $data$
+
+#### Hora: $hora$
+
+#### Local: $local$
+
+#### Banca Examinadora:
+
+#### Orientador(a): $orientador$
+
+#### Membros: $membros$
